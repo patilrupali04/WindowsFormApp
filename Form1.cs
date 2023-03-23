2e631d8f77360829624205816323c36b470b05da
Rupali Patil <125479555+patilrupali04@users.noreply.github.com>
2023-03-23 11:20:42 -0700

    Add files via upload
    connection code

---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GridViewTable
+{
+    public partial class Form1 : Form
+    {
+        string conectionString = @"Data Source=DESKTOP-RKENVIH\SQLEXPRESS; Initial Catalog =Test; Integrated Security=True";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sqlQuery = "Select * from Employee";
+            using (SqlConnection con = new SqlConnection(conectionString))
+            {
+                con.Open();
+                SqlCommand sqlCommand = new SqlCommand(sqlQuery, con);
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(sqlCommand);
+                DataTable dataTable = new DataTable();
+                dataAdapter.Fill(dataTable);
+
+                dataGridView1.DataSource = dataTable;
+
+            }
+        }
+    }
+}
